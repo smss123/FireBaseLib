bb44490301eb8b95608c0a3004c3f180e56ef8b4
pexel systems <smsssamer@live.com>
2019-03-25 21:29:57 +0200

    * Program.cs: * AssemblyInfo.cs:
    * Examples.csproj: Create Examples for database

---
+++ FireBaseLib/Examples/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Examples
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Instanciating with base URL  
+            FirebaseDB firebaseDB = new FirebaseDB("{URL OF DATABASE HERE}");
+            // Referring to Node with name "Teams"  
+            FirebaseDB users = firebaseDB.Node("Users");
+
+            var data = @"{  
+'UserName':'samer',
+'Password':'mypassword',
+'Type':'Admin'
+                          }";
+
+            FirebaseResponse getResponse = users.Get();
+
+            WriteLine(getResponse.Success);
+            if (getResponse.Success)
+                WriteLine(getResponse.JSONContent);
+            WriteLine();
+
+            WriteLine("PUT Request");
+            FirebaseResponse putResponse = users.Put(data);
+            WriteLine(putResponse.Success);
+            WriteLine();
+
+            WriteLine("POST Request");
+            FirebaseResponse postResponse = users.Post(data);
+            WriteLine(postResponse.Success);
+            WriteLine();
+
+            WriteLine("PATCH Request");
+            FirebaseResponse patchResponse = users
+                // Use of NodePath to refer path lnager than a single Node  
+                .NodePath("users/M1")
+                .Patch("{\"Type\":\"admin\"}");
+            WriteLine(patchResponse.Success);
+            WriteLine();
+
+            WriteLine("DELETE Request");
+            FirebaseResponse deleteResponse = users.Delete();
+            WriteLine(deleteResponse.Success);
+            WriteLine();
+
+            WriteLine(users.ToString());
+            ReadLine();
+
+
+        }
+    }
+}
