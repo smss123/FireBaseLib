f7c5fb7658ed7e1650bc531be968977f7e685c3c
pexel systems <smsssamer@live.com>
2019-03-25 21:06:51 +0200

    * BaseCore.cs: update database area ,i well keep working
    * FirebaseDB.cs:
    * UtilityHelper.cs:
    * FirebaseRequest.cs:
    * FirebaseResponse.cs:

--- FireBaseLib/FireBaseLib/Core/BaseCore.cs
+++ FireBaseLib/FireBaseLib/Core/BaseCore.cs
@@ -3,6 +3,10 @@ namespace FireBaseLib.Core
 {
     public class BaseCore
     {
+        public string FireBaseUrl { get; set; }
+        public string FireBaseNotificationUrl { get; set; }
+
+
         public BaseCore()
         {
         }
